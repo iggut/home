6f77f32528ed9ddca5da34d93d8e51fb84fcbf07
Igor G <igor.gutchin@gmail.com>
2023-07-21 12:02:29 -0400

    sss

--- system/users/main.nix
+++ system/users/main.nix
@@ -1,21 +1,36 @@
-{ config, lib, ... }:
-
+{
+  config,
+  lib,
+  ...
+}:
 lib.mkIf config.main.user.enable {
-	users.users.${config.main.user.username} = {
-		createHome = true;
-		home = "/home/${config.main.user.username}";
-		useDefaultShell = true;
-		password = "1"; # Default password used for first login, change later with passwd
-		isNormalUser = true;
-		description = "${config.main.user.description}";
-		extraGroups = [ "networkmanager" "wheel" "kvm" "docker" "input" ];
-	};
+  users.users.${config.main.user.username} = {
+    createHome = true;
+    home = "/home/${config.main.user.username}";
+    useDefaultShell = true;
+    password = "1"; # Default password used for first login, change later with passwd
+    isNormalUser = true;
+    description = "${config.main.user.description}";
+    extraGroups = ["networkmanager" "wheel" "kvm" "docker" "input"];
+  };
 
-	home-manager.users.${config.main.user.username}.home = {
-		stateVersion = config.state-version;
-		file.".nix-successful-build" = {
-			text = "true";
-			recursive = true;
-		};
-	};
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "nvidia-persistenced"
+      "nvidia-x11"
+      "nvidia-settings"
+      "1password"
+      "1password-cli"
+      "steam"
+      "steam-run"
+      "steam-original"
+    ];
+
+  home-manager.users.${config.main.user.username}.home = {
+    stateVersion = config.state-version;
+    file.".nix-successful-build" = {
+      text = "true";
+      recursive = true;
+    };
+  };
 }
