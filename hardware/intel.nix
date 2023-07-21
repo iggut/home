a8654f8d82a1baf4a3bddbca5473d95900da76de
Igor G <igor.gutchin@gmail.com>
2023-07-21 14:51:03 -0400

    no  liky

--- hardware/intel.nix
+++ hardware/intel.nix
@@ -1,6 +1,4 @@
-{ config, ... }:
-
-{
-	hardware.cpu.intel.updateMicrocode = config.intel.enable;
-	services.throttled.enable = config.intel.enable;
+{config, ...}: {
+  hardware.cpu.intel.updateMicrocode = config.intel.enable;
+  #services.throttled.enable = config.intel.enable;
 }
