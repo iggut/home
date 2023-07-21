180a03d55b6eca2c632ef193693dddcfc471decf
Igor G <igor.gutchin@gmail.com>
2023-07-21 14:49:01 -0400

    homuuuuu

--- hardware/nvidia.nix
+++ hardware/nvidia.nix
@@ -14,7 +14,7 @@ lib.mkIf config.nvidia.enable {
   environment.systemPackages = [pkgs.nvtop-nvidia]; # Monitoring tool for nvidia GPUs
 
   #Hyprland
-  home = {
+  home-manager = {
     sessionVariables = {
       #EDITOR = "code";
       #BROWSER = "brave";
