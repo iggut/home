c6b1a682bd39e522d630ac8dc10904cb69a2869a
Igor G <igor.gutchin@gmail.com>
2023-07-21 20:39:13 -0400

    t2

--- hardware/nvidia.nix
+++ hardware/nvidia.nix
@@ -21,22 +21,22 @@ lib.mkIf config.nvidia.enable {
       TERMINAL = "kitty";
       #LAUNCHER = "nwg-drawer";
 
-      #QT_QPA_PLATFORMTHEME = "gnome";
-      #QT_SCALE_FACTOR = "1";
+      QT_SCALE_FACTOR = "1";
       MOZ_ENABLE_WAYLAND = "1";
-      #SDL_VIDEODRIVER = "wayland";
-      #_JAVA_AWT_WM_NONREPARENTING = "1";
-      #QT_QPA_PLATFORM = "wayland";
-      #QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-      #QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      SDL_VIDEODRIVER = "wayland";
+      _JAVA_AWT_WM_NONREPARENTING = "1";
+      QT_QPA_PLATFORM = "wayland";
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
 
       WLR_NO_HARDWARE_CURSORS = "1"; # if no cursor,uncomment this line
-      #WLR_RENDERER_ALLOW_SOFTWARE = "1";
-      GBM_BACKEND = "nvidia-drm";
+      WLR_RENDERER_ALLOW_SOFTWARE = "1";
+      # GBM_BACKEND = "nvidia-drm";
       CLUTTER_BACKEND = "wayland";
       __GLX_VENDOR_LIBRARY_NAME = "nvidia";
       LIBVA_DRIVER_NAME = "nvidia";
       WLR_RENDERER = "vulkan";
+      # __NV_PRIME_RENDER_OFFLOAD = "1";
     };
   };
 
