2d103fe8364e1d3cfc6d5e670cf4e4e5bc5c5412
Igor G <igor.gutchin@gmail.com>
2023-07-23 22:21:33 -0400

    lp

--- hardware/laptop.nix
+++ hardware/laptop.nix
@@ -1,12 +1,22 @@
-{ pkgs, lib, config, ...}:
-
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 lib.mkIf config.laptop.enable {
-	services.auto-cpufreq.enable = true;
-	environment.systemPackages = [ pkgs.brightnessctl ];
+  services.auto-cpufreq.enable = true;
+  environment.systemPackages = [pkgs.brightnessctl];
 
-	hardware.nvidia.prime = lib.mkIf config.nvidia.enable {
-		offload.enable = true;
-		intelBusId = "PCI:0:2:0";
-		nvidiaBusId = "PCI:1:0:0";
-	};
+  nvidia = {
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      reverseSync.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
 }
